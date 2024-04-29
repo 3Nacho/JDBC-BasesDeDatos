a2d077df7e3919b37b2b12e3ba9d5ebbc9123adc
3Nacho <150005496+3Nacho@users.noreply.github.com>
2024-04-29 10:41:01 +0200

    Añado comentarios (para el método comprarBillete) y nombres de los autores en el código

--- ServicioImpl.java
+++ ServicioImpl.java
@@ -13,6 +13,15 @@ import org.slf4j.LoggerFactory;
 import lsi.ubu.excepciones.CompraBilleteTrenException;
 import lsi.ubu.util.PoolDeConexiones;
 
+/**
+ * ServicioImpl: Implementa los métodos para comprar y anular billetes.
+ * 
+ * @author <a href="mailto:cnd1003@alu.ubu.es">Christian Núñez</a>
+ * @author <a href="mailto:dhp1005@alu.ubu.es">Daniel Hermelo</a>
+ * @author <a href="mailto:izg1002@alu.ubu.es">Ignacio Zaldo</a>
+ * @version 1.0
+ * @since 1.0
+ */
 public class ServicioImpl implements Servicio {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ServicioImpl.class);
 
@@ -91,11 +100,14 @@ public class ServicioImpl implements Servicio {
 	    PreparedStatement st = null;
 	    ResultSet rs = null;
 	    
+	    // Convertimos las fechas y horas a formatos adecuados para la base de datos
 	    java.sql.Date fechaSqlDate = new java.sql.Date(fecha.getTime());
 		java.sql.Timestamp horaTimestamp = new java.sql.Timestamp(hora.getTime());
 		
 	    try {
 	        con = PoolDeConexiones.getInstance().getConnection();
+	        
+	        // Consultamos si hay disponibilidad de plazas para el viaje elegido
 	        String query = "SELECT idViaje, nPlazasLibres, precio FROM viajes JOIN recorridos ON viajes.idRecorrido = recorridos.idRecorrido " +
 	            "WHERE recorridos.estacionOrigen = ? AND recorridos.estacionDestino = ? AND " +
 	            "viajes.fecha = ? AND TO_CHAR(recorridos.horaSalida, 'HH24:MI') = TO_CHAR(?, 'HH24:MI')";
@@ -105,17 +117,21 @@ public class ServicioImpl implements Servicio {
 	        st.setDate(3, new java.sql.Date(fecha.getTime()));
 	        st.setTime(4, hora);
 	        rs = st.executeQuery();
+	        
+	        // Verificamos que el viaje exista y que tenga suficientes plazas disponibles
 	        if (!rs.next()) {
 	        	con.rollback();
-	            throw new CompraBilleteTrenException(2);
+	            throw new CompraBilleteTrenException(2); // En caso de no tener disponibilidad
 	        }
 	        int plazasDisponibles = rs.getInt("nPlazasLibres");
 	        int idViaje = rs.getInt("idViaje");
 	        float precio = rs.getFloat("precio");
 	        if (plazasDisponibles < nroPlazas) {
 	        	con.rollback();
-	            throw new CompraBilleteTrenException(1);
+	            throw new CompraBilleteTrenException(1); // En caso de no tener suficientes plazas
 	        }
+	        
+	        // Insertamos un nuevo ticket dentro de la base de datos
 	        PreparedStatement stInsert = con.prepareStatement("INSERT INTO tickets (idTicket, idViaje, fechaCompra, cantidad, precio)"
 					+ " VALUES (seq_tickets.NEXTVAL, ?, CURRENT_DATE, ?, ?)");
 				stInsert.setInt(1, idViaje);
@@ -124,7 +140,7 @@ public class ServicioImpl implements Servicio {
 				int filas = stInsert.executeUpdate();			
 				if (filas == 1) con.commit();
 				
-				// Actualizar la cantidad de plazas disponibles
+				// Actualizamos la cantidad de plazas disponibles para el viaje en concreto
 				String updateQuery = "UPDATE viajes SET nPlazasLibres = nPlazasLibres - ? WHERE idViaje = ?";
 				PreparedStatement updateSt = con.prepareStatement(updateQuery);
 				updateSt.setInt(1, nroPlazas);
@@ -134,10 +150,11 @@ public class ServicioImpl implements Servicio {
 	        
 	        con.commit();
 	    } catch (SQLException e) {
-	        LOGGER.error("SQL Error: ", e); // La línea esta la podríamos comentar para que no salga texto de más al ejecutar los test
-	        if (con != null) con.rollback();
-	        throw e;
+	        LOGGER.error("SQL Error: ", e); // La línea esta la podríamos comentar para que no salga un texto largo al ejecutar los test
+	        if (con != null) con.rollback(); // Si hay algún error, hacemos un rollback para deshacer los cambios
+	        throw e;  // Lanzamos la excepción
 	    } finally {
+	    	// Cerramos el resultset, la conexión y preparedstatements
 	        if (rs != null) rs.close();
 	        if (st != null) st.close();
 	        if (con != null) con.close();
